369ec9f74192907edaf0d9e00405356b4ef8bbbd
Ziad Mahmoud <ziadmahmoudx98@gmail.com>
2024-03-03 22:32:06 +0200

    The Final Commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <nav className="bg-sky-800 text-white px-4 py-2 flex  items-center mb-4">
       <a className="flex px-4 " href="/">
-        <MdImageSearch className="scale-150 mr-2 mt-1" />
-        <h1 className="text-xl font-bold">Search Photos</h1>
+        <h1 className="text-xl font-bold">SnapHunt</h1>
+        <MdImageSearch className="scale-150 ml-1.5 mt-1.5" />
       </a>
     </nav>
   );
